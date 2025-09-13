230d61ef091c67602441e42d5336d7cab6c48f52
Astro <116870775+sonicpanther101@users.noreply.github.com>
2025-09-13 16:27:00 +1200

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import NavbarLayout from "./layout";
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-document.documentElement.classList.toggle(
-  "dark",
-  localStorage.theme === "dark" ||
-    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-);
+//document.documentElement.classList.toggle(
+//  "dark",
+//  localStorage.theme === "dark" ||
+//    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+//);
 
 export default function Home() {
   return (
