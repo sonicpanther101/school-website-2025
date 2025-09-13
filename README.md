e95cc8c596201037cb28301f607798fa7a8e685f
sonicpanther101 <sonicpanther101@gmail.com>
2025-09-13 16:45:12 +1200

    added footer design

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ react framework - [NextJS](https://nextjs.org/docs/app/getting-started/installat
 
 hero page design - [https://prismic.io/blog/website-hero-section](https://prismic.io/blog/website-hero-section)
 
+navbar and footer design ideas - [Figma](https://www.figma.com)
+
 palette - [catppuccin](https://github.com/catppuccin/tailwindcss)
 
 styling - [tailwind](https://tailwindcss.com/docs/)
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
-import NavbarLayout from "./layout";
+"use client"
 
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-document.documentElement.classList.toggle(
-  "dark",
-  localStorage.theme === "dark" ||
-    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-);
+import NavbarLayout from "./layout";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Image, Link } from "@heroui/react";
+
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// document.documentElement.classList.toggle(
+//   "dark",
+//   localStorage.theme === "dark" ||
+//     (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+// );
 
 export default function Home() {
   return (
-    <div>
-      {NavbarLayout()}
-    </div>
+    <html>
+      <body>
+        {NavbarLayout()}
+        <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="heroui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">HeroUI</p>
+          <p className="text-small text-default-500">heroui.com</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <p>Make beautiful websites regardless of your design experience.</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+      </body>
+    </html>
   );
 }
