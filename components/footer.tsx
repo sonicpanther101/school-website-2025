f26355dce47843ad75e117ea6d128e0129e76cc6
sonicpanther101 <sonicpanther101@gmail.com>
2025-09-19 11:40:08 +1200

    styling fix for footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,18 +4,20 @@ export const Footer = () => {
   return (
     <Card className="flex flex-col sm:flex-row p-[10px] gap-3">
       <Card className="flex-2">
-        <CardHeader>
-          <Image
-            className="flex-none"
-            alt="Marlborough tramps logo"
-            radius="sm"
-            src="https://raw.githubusercontent.com/sonicpanther101/school-website-2025/refs/heads/main/Images/Marlborough%20Tramps%20logo%20large.jpg"
-            width={120}
-          />
-        </CardHeader>
         <CardBody className="flex flex-col gap-3">
-          <p>Contact Us</p>
-          <p>Email: <Link isExternal showAnchorIcon underline="active" href="mailto:admin@marlboroughtramps.co.nz">admin@marlboroughtramps.co.nz</Link></p>
+          <div className="flex flex-col sm:flex-row gap-3">
+            <Image
+              className="flex-none"
+              alt="Marlborough tramps logo"
+              radius="sm"
+              src="https://raw.githubusercontent.com/sonicpanther101/school-website-2025/refs/heads/main/Images/Marlborough%20Tramps%20logo%20large.jpg"
+              width={100}
+            />
+            <div className="flex flex-col">
+              <p>Contact Us</p>
+              <p>Email: <Link isExternal showAnchorIcon underline="active" href="mailto:admin@marlboroughtramps.co.nz">admin@marlboroughtramps.co.nz</Link></p>
+            </div>
+          </div>
           <div className="flex flex-col sm:flex-row gap-3">
             <div className="flex flex-col">
               <p>Blenheim Office</p>
